f95073bd70664cc422bf14547b80984ff94ee571
Danielle Santos <daniellerodris@gmail.com>
2020-05-15 20:08:09 -0300

    Criação da interface de viagem

--- lib/screens/PassengerPanel.dart
+++ lib/screens/PassengerPanel.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'dart:async';
+import 'dart:io';
 
 class PassengerPanel extends StatefulWidget {
   @override
@@ -108,12 +109,99 @@ class _PassengerPanelState extends State<PassengerPanel> {
         ],
       ),
       body: Container(
-        child: GoogleMap(
-          mapType: MapType.normal,
-            initialCameraPosition: _cameraPosition,
-          onMapCreated: _onMapCreated,
-          myLocationEnabled: true,
-        ),
+        child: Stack(
+          children: <Widget>[
+            GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: _cameraPosition,
+              onMapCreated: _onMapCreated,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: false,
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(3),
+                    color: Colors.white
+                  ),
+                  child: TextField(
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      icon: Container(
+                        margin: EdgeInsets.only(left: 20),
+                        width: 10,
+                        height: 10,
+                        child: Icon(Icons.location_on, color: Colors.green,),
+                      ),
+                      hintText: "Meu local",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 5, top: 10)
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 55,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(3),
+                      color: Colors.white
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        icon: Container(
+                          margin: EdgeInsets.only(left: 20),
+                          width: 10,
+                          height: 10,
+                          child: Icon(Icons.local_taxi, color: Colors.black,),
+                        ),
+                        hintText: "Digite o destino",
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.only(left: 5, top: 10)
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              right: 0,
+              left: 0,
+              bottom: 0,
+              child: Padding(
+                padding: Platform.isIOS
+                  ? EdgeInsets.fromLTRB(20, 10, 20, 25)
+                  : EdgeInsets.all(10),
+                child: RaisedButton(
+                    child: Text(
+                      "Chamar Uber",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    color: Color(0xff1ebbd8),
+                    padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    onPressed: (){
+
+                    }
+                ),
+              ),
+            )
+          ],
+        )
       ),
     );
   }
