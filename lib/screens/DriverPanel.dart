8b4fd461d10df062263ed25713099617b9d69bd9
Danielle Santos <daniellerodris@gmail.com>
2020-05-18 18:20:50 -0300

    Listando requisições para o motorista

--- lib/screens/DriverPanel.dart
+++ lib/screens/DriverPanel.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:uber/util/StatusRequest.dart';
 
 class DriverPanel extends StatefulWidget {
   @override
@@ -11,6 +15,8 @@ class _DriverPanelState extends State<DriverPanel> {
   List<String> itemsMenu = [
     "Configurations", "LogOut"
   ];
+  final _controller = StreamController<QuerySnapshot>.broadcast();
+  Firestore db = Firestore.instance;
 
   _logOutUser() async {
     FirebaseAuth auth = FirebaseAuth.instance;
@@ -29,8 +35,45 @@ class _DriverPanelState extends State<DriverPanel> {
     }
   }
 
+  Stream<QuerySnapshot> _addListenerRequests(){
+    final stream = db.collection("requests")
+        .where("status", isEqualTo: StatusRequest.WAITING)
+        .snapshots();
+
+    stream.listen((data){
+      _controller.add(data);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _addListenerRequests();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    var messageLoading = Center(
+      child: Column(
+        children: <Widget>[
+          Text("Carregando requisições"),
+          CircularProgressIndicator()
+        ],
+      ),
+    );
+
+    var messageHasNoData = Center(
+      child: Text(
+        "Você não tem nenhuma requisição :(",
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Painel Motorista"),
@@ -48,7 +91,50 @@ class _DriverPanelState extends State<DriverPanel> {
           )
         ],
       ),
-      body: Container(),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _controller.stream,
+        builder: (context, snapshot){
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return messageLoading;
+              break;
+            case ConnectionState.active:
+            case ConnectionState.done:
+              if(snapshot.hasError){
+                return Text("Erro ao carregar os dados.");
+              } else {
+                QuerySnapshot querySnapshot = snapshot.data;
+                if(querySnapshot.documents.length == 0){
+                  return messageHasNoData;
+                } else {
+                  return ListView.separated(
+                    itemCount: querySnapshot.documents.length,
+                    separatorBuilder: (context, index) => Divider(
+                      height: 2,
+                      color: Colors.grey,
+                    ),
+                    itemBuilder: (context, index){
+                      List<DocumentSnapshot> requests = querySnapshot.documents.toList();
+                      DocumentSnapshot item = requests[index];
+
+                      String idRequest = item["id"];
+                      String passengerName = item["passenger"]["name"];
+                      String street = item["destiny"]["street"];
+                      String number = item["destiny"]["number"];
+
+                      return ListTile(
+                        title: Text(passengerName),
+                        subtitle: Text("Destino: $street, $number"),
+                      );
+                    },
+                  );
+                }
+              }
+              break;
+          }
+        }
+      ),
     );
   }
 }
